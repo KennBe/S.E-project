5575f4737178bda2dea863d6727d4fbddcc757fb
KathleenTupaz <126863273+KathleenTupaz@users.noreply.github.com>
2023-04-25 22:13:55 +0800

    Buttons are now functional
    Added the pages, which are now connected to its respective buttons.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog, QApplication 
+from PyQt6.QtWidgets import QDialog, QApplication , QStackedWidget, QWidget
 from PyQt6.uic import loadUi
 import sys
 
@@ -6,6 +6,21 @@ class Window(QDialog):
     def __init__(self):
         super(Window, self).__init__()
         loadUi("main.ui", self)
+        self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+
+        #Buttons
+        self.addToDoBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.ToDoPage))
+        self.addNoteBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.NotesPage))
+        self.addAlarmBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.AlarmPage))
+        self.addReminderBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.ReminderPage))
+        
+
+    def calendarDateChanged(self):
+        print("The calendar date was changed.")
+        dateSelected = self.calendarWidget.selectedDate().toPyDate()
+        print("Date selected:", dateSelected)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

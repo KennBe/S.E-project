127e692e57afe7d938e791612cfca8db9f4b9c8c
KennBe <beltrankenneth76@gmail.com>
2023-04-27 00:57:40 +0800

    additional code for UI,and unfinished code for alarmPage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QDialog, QApplication , QStackedWidget, QWidget
 from PyQt6.uic import loadUi
 import sys
+import datetime
 
 class Window(QDialog):
     def __init__(self):
@@ -9,16 +10,33 @@ class Window(QDialog):
         self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
 
         #Buttons
+        self.stackedWidget.setCurrentWidget(self.main)
         self.addToDoBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.ToDoPage))
         self.addNoteBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.NotesPage))
         self.addAlarmBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.AlarmPage))
         self.addReminderBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.ReminderPage))
-        
 
+        
+        #ToDoList
     def calendarDateChanged(self):
         print("The calendar date was changed.")
         dateSelected = self.calendarWidget.selectedDate().toPyDate()
         print("Date selected:", dateSelected)
+
+        #alarmpage
+    def ToSetAnAlarm(self):
+
+        alarmHour = int(input("Enter hour: "))
+        alarmMin = int(input("Enter Minutes: "))
+        alarmAm = input("am / pm")
+
+        if alarmAm=="pm":  
+            alarmHour+=12
+    
+        while True:
+            if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute:
+                print("WAKE UP")
+                break
 
 
 

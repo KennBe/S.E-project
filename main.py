99d6a395c43e2d14b93400b34297c688933bb646
KennBe <beltrankenneth76@gmail.com>
2023-05-10 01:30:54 +0800

    Working alarm with sounds

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from PyQt6.QtWidgets import QApplication, QDialog, QLCDNumber, QListWidgetItem
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer, QUrl
+from PyQt6.QtMultimedia import QSoundEffect
 from PyQt6.uic import loadUi
 from datetime import datetime
 import sys
-from PyQt6.QtCore import QUrl
-from PyQt6.QtMultimedia import QSoundEffect
+
+
 
 class Window(QDialog):
     def __init__(self):
-        super(Window, self).__init__()
+        super().__init__()
 
         # Load the main UI file
         loadUi("main.ui", self)
@@ -24,30 +25,31 @@ class Window(QDialog):
         self.addReminderBtn.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.ReminderPage))
 
         # Call the __lcd__ function to start the LCD clock
-        self.__lcd__()
-
-        # Set up the alarm page
+        self.lcdclock()
+       
+        # alarm page buttons
         self.addAlarmTimeButton.clicked.connect(self.addAlarm)
         self.removeAlarmButton.clicked.connect(self.removeAlarm)
         self.alarmList.itemDoubleClicked.connect(self.removeAlarm)
 
         # Create a sound effect for the alarm
-        self.alarmSound = QSoundEffect()
-        self.alarmSound.setSource(QUrl.fromLocalFile("alarm.wav"))
-    
+        self.alarmSound = QSoundEffect(self)
+        self.alarmSound.setSource(QUrl.fromLocalFile("alarm1.wav"))
+        self.alarmSound.setVolume(1.0)
+
     # ToDoList
     def calendarDateChanged(self):
         print("The calendar date was changed.")
-        dateSelected = self.calendarWidget.selectedDate().toPyDate()
+        dateSelected = self.calendarWidget.selectedDate().toPython()
         print("Date selected:", dateSelected)
 
     # Alarm page/LCD clock
-    def __lcd__(self):
+    def lcdclock(self):
         # Find the QLCDNumber widget
         self.lcd = self.findChild(QLCDNumber, "lcdNumber")
 
         # Create a timer and connect it to the lcd_number slot
-        self.timer = QTimer()
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.lcd_number)
 
         # Start the timer and update every second
@@ -78,21 +80,18 @@ class Window(QDialog):
 
                 # Remove the alarm from the list
                 self.alarmList.takeItem(self.alarmList.row(item))
+                print(current_time)
 
-
-    # Add an alarm to the list
+    # Add an alarm to the alarm list
     def addAlarm(self):
-        time = self.alarmTimeEdit.time().toString("hh:mm:ss ap")
+        time = self.alarmTimeEdit.time().toString("hh:mm:ss AP")
         alarm = QListWidgetItem(time)
         self.alarmList.addItem(alarm)
 
-    # Remove an alarm from the list
+    # Remove an alarm from the alarm list
     def removeAlarm(self):
-        selected = self.alarmList.selectedItems()
-        if not selected: return
-        for item in selected:
+        for item in self.alarmList.selectedItems():
             self.alarmList.takeItem(self.alarmList.row(item))
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
